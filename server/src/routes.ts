924ffba2cb4349b34e77eabc775c9d16598b1731
tonhus <antonio_abrantes@hotmail.com.br>
2020-06-04 16:15:53 -0300

    dia 02 - finalização

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 
+//Controllers
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-  return response.send("Home new!");
-});
+routes.get('/items', itemsController.index );
 
+routes.post('/points', pointsController.create );
+routes.get('/points', pointsController.index );
+routes.get('/points/:id', pointsController.show );
 
 export default routes;
